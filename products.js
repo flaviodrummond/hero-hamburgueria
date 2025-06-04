2afb94c61cc866ceea006d50c9a9c1511f43e746
flaviodrummond <flaviosax20@gmail.com>
2025-06-04 02:37:06 -0300

    Modificado preços no products.js e criado um novo arquivo js, onde iremos colocar nosso código

--- products.js
+++ products.js
@@ -2,14 +2,14 @@ const menuOptions = [
 
 { name: 'Spider-Man', price: 29.90, vegan: false, src: './assets/spider-man.png' },
 
-{ name: 'Hulk', price: 44.90, vegan: true, src: './assets/hulk.png' },
+{ name: 'Hulk', price: 52.90, vegan: true, src: './assets/hulk.png' },
 
-{ name: 'Aquaman', price: 42.90, vegan: false, src: './assets/aquaman.png' },
+{ name: 'Aquaman', price: 45.90, vegan: false, src: './assets/aquaman.png' },
 
 { name: 'Batman', price: 34.90, vegan: false, src: './assets/batman.png' },
 
-{ name: 'Thor', price: 39.90, vegan: false, src: './assets/thor-1.png' },
+{ name: 'Thor', price: 40.90, vegan: false, src: './assets/thor-1.png' },
 
-{ name: 'Tony Stark', price: 46.90, vegan: true, src: './assets/tony-stark.png' },
+{ name: 'Tony Stark', price: 55.90, vegan: true, src: './assets/tony-stark.png' },
 
 ]
