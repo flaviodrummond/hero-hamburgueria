2b29ce1a1bcac7bf63b5c455d5c6fad3dde72beb
flaviodrummond <flaviosax20@gmail.com>
2025-06-04 11:44:54 -0300

    edição no HTML, criação de uma borda em volta da img e height na img e criação do forEach na aplicação no JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log(menuOptions)
+const list = document.querySelector('ul')
+const buttonMenu = document.querySelector('.menu')
+
+let newList = ''
+
+const optionMenu = () => {
+
+    menuOptions.forEach(product => {
+    newList = newList + 
+    `
+        <li>
+            <img src= ${product.src}>
+
+            <p> ${product.name}</p>
+            <p class="value">R$ ${product.price.toFixed(2)}</p>
+        </li>
+    `
+})
+
+list.innerHTML = newList
+
+}
+
+buttonMenu.addEventListener('click', optionMenu)
