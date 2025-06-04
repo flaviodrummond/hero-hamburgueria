363c24c90ea2d0c915edec6080257164b99e21aa
flaviodrummond <flaviosax20@gmail.com>
2025-06-03 22:59:42 -0300

    Ligando o HTML no JavaScripit e criando os produtos no Js

---
+++ products.js
@@ -0,0 +1,15 @@
+const menuOptions = [
+
+{ name: 'Spider-Man', price: 29.90, vegan: false, src: './assets/spider-man.png' },
+
+{ name: 'Hulk', price: 44.90, vegan: true, src: './assets/hulk.png' },
+
+{ name: 'Aquaman', price: 42.90, vegan: false, src: './assets/aquaman.png' },
+
+{ name: 'Batman', price: 34.90, vegan: false, src: './assets/batman.png' },
+
+{ name: 'Thor', price: 39.90, vegan: false, src: './assets/thor-1.png' },
+
+{ name: 'Tony Stark', price: 46.90, vegan: true, src: './assets/tony-stark.png' },
+
+]
