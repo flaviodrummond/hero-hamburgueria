8a248d8eafe41a9c4e68392777bef916c2666c85
flaviodrummond <flaviosax20@gmail.com>
2025-06-06 10:50:53 -0300

    Criado o filter no array da nossa aplicação para pegar os produtos mais vendidos e mostrar na telaa para o cliente final

--- products.js
+++ products.js
@@ -1,15 +1,15 @@
 const menuOptions = [
 
-{ name: 'Spider-Man', price: 29.90, vegan: false, src: './assets/spider-man.png' },
+{ name: 'Spider-Man', price: 29.90, bestSellers: false, src: './assets/spider-man.png' },
 
-{ name: 'Hulk', price: 52.90, vegan: true, src: './assets/hulk.png' },
+{ name: 'Hulk', price: 52.90, bestSellers: true, src: './assets/hulk.png' },
 
-{ name: 'Aquaman', price: 45.90, vegan: false, src: './assets/aquaman.png' },
+{ name: 'Aquaman', price: 45.90, bestSellers: false, src: './assets/aquaman.png' },
 
-{ name: 'Batman', price: 34.90, vegan: false, src: './assets/batman.png' },
+{ name: 'Batman', price: 34.90, bestSellers: false, src: './assets/batman.png' },
 
-{ name: 'Thor', price: 40.90, vegan: false, src: './assets/thor-1.png' },
+{ name: 'Thor', price: 40.90, bestSellers: false, src: './assets/thor-1.png' },
 
-{ name: 'Tony Stark', price: 55.90, vegan: true, src: './assets/tony-stark.png' },
+{ name: 'Tony Stark', price: 55.90, bestSellers: true, src: './assets/tony-stark.png' },
 
 ]
--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const list = document.querySelector('ul')
 const buttonMenu = document.querySelector('.menu')
 const buttonDiscount = document.querySelector('.discount')
+const buttonTotalValue = document.querySelector('.total-value')
+const buttonFilter = document.querySelector('.search')
 
 let newList = ''
 
+
+
+// Criação do ForEach 
 const optionMenu = (productsArray) => {
 
     newList = ''
@@ -15,7 +20,7 @@ const optionMenu = (productsArray) => {
             <img src= ${product.src}>
 
             <p> ${product.name}</p>
-            <p class="value">R$ ${product.price.toFixed(2)}</p>
+            <p class="value"> ${currency (product.price.toFixed(2))}</p>
         </li>
     `
     })
@@ -23,6 +28,7 @@ const optionMenu = (productsArray) => {
     list.innerHTML = newList
 }
 
+// Criação do Map, pegar cada item da nossa lista array e dá um desconto de 10% em cada produto da lista e colocar na tela para o usuário final. 
 const newPrice = () => {
     const discountedPrice = menuOptions.map((product) => ({
         ...product,
@@ -31,8 +37,33 @@ const newPrice = () => {
     optionMenu(discountedPrice)
 }
 
+// Criação do reduce para pegar todos os produtos existentes em nossa lista e colocar o valor total na tela para o usuário final.
+const finalPrice = () => {
+    const totalPrice = menuOptions.reduce((acc, value) => {
+        return acc + value.price;
+    }, 0)
+
+    list.innerHTML =
+        `
+        <li>
+            <p> O valor total de todos os produtos é de ${currency (totalPrice.toFixed(2))}</p>
+        </li>
+    `
+
+}
+
+// Criação do filter para filtrar os produtos mais pedidos pelos clientes do nosso array e colocar na tela para o cliente final
+const search = () => {
+    const salesChampions = menuOptions.filter(product => product.bestSellers === true) 
+
+    optionMenu(salesChampions)
+}
+
+
 
 
 
 buttonMenu.addEventListener('click', () => optionMenu(menuOptions))
 buttonDiscount.addEventListener('click', newPrice)
+buttonTotalValue.addEventListener('click', finalPrice)
+buttonFilter.addEventListener('click', search)
